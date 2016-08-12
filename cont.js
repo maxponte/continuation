82f4dc1d15a1a25dd035b1a808b6c69dd432d1c7
maxponte <mponte@blendlabs.com>
2016-08-12 01:36:29 -0700

    update

--- cont.js
+++ cont.js
@@ -1,34 +1,56 @@
-//intEq : int * int -> bool
-function intEq (x,y) { return x == y; }
-//intWin : unit -> int list
-function intWin () { return []; }
-//intLose : unit -> bool
-function intLose () { return false; }
-//ghettoCons : 'a * 'a list -> 'a list
-function ghettoCons (x, L) { return [x].concat(L); }
-//isEmpty : object -> bool
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
+const log = (x) => {
+  console.log(x);
+  return x;
 }
 
-//findMyPath : 'a * 'a tree * (unit -> bool) * (unit -> int list)
-//ENSURES if me is in tree, then findMyPath returns the path of me.
-//Otherwise, findMyPath returns false.
-
-function findMyPath (me, tree, lose, win) {
-  //console.log("me: "+me+"\ntree: "+JSON.stringify(tree)+"\nlose: "+lose()+"\nwin: "+win());
-  if(isEmpty(tree)) { return lose(); }
-  if(intEq(me, tree.val)) { return win(); }
-  return findMyPath (me, tree.left, function () {
-    return findMyPath (me, tree.right, lose, function() { return ghettoCons (1, win()); });
-  }, function() {
-    return ghettoCons (0, win());
-  });
+const RandomTree = ({iterator}) => {
+    const arrayToTree = arr => (p =>
+        arr.length ? {
+            left: arrayToTree(arr.slice(0, p)),
+            val: arr[p],
+            right: arrayToTree(arr.slice(p + 1))
+        } : null
+    )(
+        ~~(arr.length * Math.random())
+    )
+    const make = () => arrayToTree(Array.from(iterator()))
+    return { make, empty }
 }
 
-//sample tree
-var a = {left : {left : {left : {}, val : 5, right : {}}, val : 2, right : {}}, val : 3, right : {left : {}, val : 1, right : {left : {}, val : 10, right : {}}}};
-console.log(findMyPath(10,a,intLose,intWin));
+const GetTreeSearchPaths = () => {
+    const eq = (x, y) => x === y
+    const success = () => []
+    const fail = () => null
+    const prepend = (x, L) => [x, ...L]
+    const empty = o => o === null
+    const findH = ({me, tree, fail, success}) =>
+        empty(tree) ? fail() :
+            eq(me, log(tree.val)) ? success() :
+                findH({
+                    me,
+                    tree: tree.left,
+                    fail: () => findH({
+                        me,
+                        tree: tree.right,
+                        fail,
+                        success: () => prepend(1, success())
+                    }),
+                    success: () => prepend(0, success())
+                })
+    const find = ({me, tree}) => findH({me: log(me), tree: log(tree), success, fail})
+    return { find }
+}
 
-function addtwo (x) { return x + 2; }
-console.log([1,2,3,4].map(addtwo));
+const { find } = GetTreeSearchPaths()
+const { make } = RandomTree({
+    iterator: function*() {
+      const expectedSize = 10;
+      const maximumValue = 11;
+      while (Math.random() > 1/expectedSize) yield ~~(maximumValue * Math.random());
+    }
+})
+log(find({me: 1, tree: make()}))
+log(find({
+    me: 10,
+    tree: {left : {left : {left : null, val : 5, right : null}, val : 2, right : null}, val : 3, right : {left : null, val : 1, right : {left : null, val : 10, right : null}}}
+}))
